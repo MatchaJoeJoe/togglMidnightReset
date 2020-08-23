2470c79baff6b635385f52fe9f445da4a216ae25
Joe Frizzell <matchajoejoe@gmail.com>
2020-08-23 17:04:41 -0400

    do nothing if no timer currently running

--- toggl Midnight Reset.app/Contents/Resources/Scripts/resetTimer.py
+++ toggl Midnight Reset.app/Contents/Resources/Scripts/resetTimer.py
@@ -4,17 +4,18 @@ import sys
 USAGE_TXT = "Usage: python [path to script] [api token]"
 
 def resetTimer(apiKey):
-	# create a Toggl object and set our API key 
+	# create a Toggl object and set our API key
 	toggl = Toggl()
 	toggl.setAPIKey(apiKey)
 
 	# get current timer info
 	currentTimer = toggl.currentRunningTimeEntry()
-	thePID = currentTimer['data']['pid']
-	theDesc = currentTimer['data']['description']
-	
-	# create duplicate timer
-	toggl.startTimeEntry(theDesc, thePID)
+	if currentTimer['data'] != None:
+		thePID = currentTimer['data']['pid']
+		theDesc = currentTimer['data']['description']
+
+		# create duplicate timer
+		toggl.startTimeEntry(theDesc, thePID)
 
 if __name__ == "__main__":
 	if len(sys.argv) == 2:
